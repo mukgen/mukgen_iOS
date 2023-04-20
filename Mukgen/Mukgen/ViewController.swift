4b7532607e1ebcd0f434562b2d9fdc2a3f7c696d
junha <goodjunha@gmail.com>
2023-04-20 21:50:33 +0900

    🔥:: main delete

--- Mukgen/Mukgen/ViewController.swift
+++ Mukgen/Mukgen/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
