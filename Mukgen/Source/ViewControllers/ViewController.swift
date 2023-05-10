0bf1d7b0f3ff2bad8aaa5be49527b483200c22d0
junha <goodjunha@gmail.com>
2023-05-10 16:55:04 +0900

    🚚:: #14 파일 경로 변경
    깃허브 보기 좋게

--- Mukgen/Source/ViewControllers/ViewController.swift
+++ Mukgen/Source/ViewControllers/ViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let button = CustomButton(title: "시작하기", backgroundColor: .primaryDark1, font: UIFont.systemFont(ofSize: 20, weight: .bold))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .pointBase
         // Do any additional setup after loading the view.
+        
+        button.center = view.center
+        view.addSubview(button)
     }
 }
-
-
