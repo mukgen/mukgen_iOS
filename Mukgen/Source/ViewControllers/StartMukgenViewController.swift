81c33583f347f75da2a8212764b1237ffda8deb8
s <tylereunholee010203@gmail.com>
2023-05-13 17:12:05 +0900

    ✨ :: 먹젠 회원가입 페이지
    먹젠 시작하기 페이지의 가장 첫 번째 뷰를 만들었어요

--- Mukgen/Source/ViewControllers/StartMukgenViewController.swift
+++ Mukgen/Source/ViewControllers/StartMukgenViewController.swift
@@ -11,12 +11,17 @@ import Then
 
 class StartMukgenViewController: UIViewController {
     
-    let startButton = CustomButton(title: "시작하기",backgroundColor: .primaryDark1,font: UIFont.systemFont(ofSize: 16, weight: .bold))
+    let startButton = CustomButton(title: "시작하기",
+                                   backgroundColor: .primaryDark1,
+                                   font: UIFont.systemFont(ofSize: 16, weight: .bold)
+    ).then {
+        $0.addTarget(self, action: #selector(startButtonDidTap), for: .touchUpInside)
+    }
     
     var startLabel = UILabel().then {
         $0.text = "시작하기"
         $0.backgroundColor = .white
-        $0.font = .systemFont(ofSize: 20, weight: .regular)
+        $0.font = .systemFont(ofSize: 24, weight: .semibold)
     }
     
     var mukgenInfo = UILabel().then {
@@ -42,34 +47,42 @@ class StartMukgenViewController: UIViewController {
         view.addSubview(mukgenInfo)
         view.addSubview(mukgenStartIcon)
         
-        startButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(view.snp.bottom).inset(34)
-            $0.width.equalTo(353)
-            $0.height.equalTo(55)
-        }
+            
+        let buttonWidth = 353
+        let buttonHeigh = 55
         
         startLabel.snp.makeConstraints {
-            $0.top.equalTo(view.snp.top).offset(123)
-            $0.left.equalTo(view.snp.left).offset(20)
-            $0.width.equalTo(83)
+            $0.top.equalToSuperview().offset(123)
+            $0.left.equalToSuperview().offset(20)
+            $0.width.equalToSuperview()
             $0.height.equalTo(29)
         }
         
         mukgenInfo.snp.makeConstraints {
-            $0.top.equalTo(view.snp.top).offset(176)
-            $0.left.equalTo(view.snp.left).offset(20)
+            $0.top.equalTo(startLabel.snp.bottom).offset(24)
+            $0.left.equalToSuperview().offset(20)
             $0.width.equalToSuperview()
             $0.height.equalTo(19)
         }
         
         mukgenStartIcon.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(view.snp.top).offset(346)
+            $0.top.equalTo(mukgenInfo.snp.bottom).offset(151)
             $0.width.height.equalTo(160)
+        }
+
+        startButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(mukgenStartIcon.snp.bottom).offset(257)
+            $0.width.equalTo(buttonWidth)
+            $0.height.equalTo(buttonHeigh)
         }
         
     }
     
-    
+    @objc func startButtonDidTap(_ sender: Any) {
+        //다음 페이지로 넘기는 코드에요
+//        self.navigationController?.pushViewController(InputNicknameController(), animated: true)
+    }
 }
+
