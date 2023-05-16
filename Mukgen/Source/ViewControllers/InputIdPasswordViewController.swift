b85d7049ad040105239091b95ea0fad4848851fa
s <tylereunholee010203@gmail.com>
2023-05-16 20:18:27 +0900

    ♻️ :: #28 아이디 비밀번호 입력 페이지 리펙토링
    비밀번호 확인 뷰 추가 및 아이디 비밀번호와 확인 코드 만듬

--- Mukgen/Source/ViewControllers/InputIdPasswordViewController.swift
+++ Mukgen/Source/ViewControllers/InputIdPasswordViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 import SnapKit
 import Then
 
-class InputIdPasswordViewController: UIViewController {
+final class InputIdPasswordViewController: UIViewController {
     
-    let inputIdPasswordLabel = UILabel().then {
+    private let inputIdPasswordLabel = UILabel().then {
         $0.numberOfLines = 2
         $0.text = "아이디와\n비밀번호를 입력해주세요."
         $0.backgroundColor = .white
         $0.font = .systemFont(ofSize: 24, weight: .semibold)
     }
     
-    let attributes = [NSAttributedString.Key.foregroundColor: UIColor.primaryLight2,
-                          .font : UIFont.systemFont(ofSize: 20, weight: .semibold)]
+    private let attributes = [NSAttributedString.Key.foregroundColor: UIColor.primaryLight2,
+                              .font : UIFont.systemFont(ofSize: 20, weight: .semibold)]
     
     internal var firstTextField = UITextField().then {
         $0.tintColor = .black
@@ -39,7 +39,16 @@ class InputIdPasswordViewController: UIViewController {
         $0.textColor = .black
     }
     
-    internal var nicknameLine = UIView().then {
+    internal var thirdTextField = UITextField().then {
+        $0.tintColor = .black
+        $0.borderStyle = UITextField.BorderStyle.none
+        $0.returnKeyType = UIReturnKeyType.done
+        $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        $0.leftViewMode = .always
+        $0.textColor = .black
+    }
+    
+    internal var idLine = UIView().then {
         $0.backgroundColor = .primaryLight2
     }
     
@@ -47,38 +56,33 @@ class InputIdPasswordViewController: UIViewController {
         $0.backgroundColor = .primaryLight2
     }
     
-    let nextPageButton = CustomButton(title: "다음",
-                                   backgroundColor: .primaryLight2,
-                                   font: UIFont.systemFont(ofSize: 16, weight: .semibold)
+    internal var checkPasswordLine = UIView().then {
+        $0.backgroundColor = .primaryLight2
+    }
+    
+    private let nextPageButton = CustomButton(title: "다음",
+                                              backgroundColor: .primaryLight2, titleColor: .white,
+                                              font: UIFont.systemFont(ofSize: 16, weight: .semibold)
     ).then {
         $0.addTarget(self, action: #selector(nextPageButtonDidTap), for: .touchUpInside)
     }
-                                      
     
-        
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
-        view.backgroundColor = .white
-        
-        lazy var textFields = [firstTextField, secondTextField]
-        let placeholders = ["아이디", "비밀번호"]
-        
-        var index = 0
-        for textField in textFields {
-            textField.attributedPlaceholder = NSAttributedString(string: placeholders[index],
-                                                                 attributes: attributes)
-            textField.delegate = self
-            index += 1
-        }
+        layout()
+        attribute()
+    }
+    
+    private func layout() {
         
         view.addSubview(inputIdPasswordLabel)
         view.addSubview(firstTextField)
-        view.addSubview(nicknameLine)
-        view.addSubview(nextPageButton)
+        view.addSubview(idLine)
         view.addSubview(secondTextField)
         view.addSubview(nextPageButton)
+        view.addSubview(thirdTextField)
+        view.addSubview(checkPasswordLine)
         view.addSubview(passwordLine)
         
         let buttonWidth = 353
@@ -98,14 +102,7 @@ class InputIdPasswordViewController: UIViewController {
             $0.height.equalTo(56)
         }
         
-        secondTextField.snp.makeConstraints() {
-            $0.top.equalTo(firstTextField.snp.bottom).offset(24)
-            $0.left.equalToSuperview().offset(20)
-            $0.width.equalToSuperview()
-            $0.height.equalTo(56)
-        }
-        
-        nicknameLine.snp.makeConstraints() {
+        idLine.snp.makeConstraints() {
             $0.top.equalTo(firstTextField.snp.bottom).offset(0)
             $0.left.equalToSuperview().offset(20)
             $0.right.equalToSuperview().inset(20)
@@ -113,8 +110,30 @@ class InputIdPasswordViewController: UIViewController {
             $0.height.equalTo(2)
         }
         
+        secondTextField.snp.makeConstraints() {
+            $0.top.equalTo(firstTextField.snp.bottom).offset(24)
+            $0.left.equalToSuperview().offset(20)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(56)
+        }
+        
         passwordLine.snp.makeConstraints() {
             $0.top.equalTo(secondTextField.snp.bottom).offset(0)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().inset(20)
+            $0.width.equalTo(352)
+            $0.height.equalTo(2)
+        }
+        
+        thirdTextField.snp.makeConstraints() {
+            $0.top.equalTo(passwordLine.snp.bottom).offset(24)
+            $0.left.equalToSuperview().offset(20)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(56)
+        }
+        
+        checkPasswordLine.snp.makeConstraints() {
+            $0.top.equalTo(thirdTextField.snp.bottom).offset(0)
             $0.left.equalToSuperview().offset(20)
             $0.right.equalToSuperview().inset(20)
             $0.width.equalTo(352)
@@ -127,27 +146,43 @@ class InputIdPasswordViewController: UIViewController {
             $0.width.equalTo(buttonWidth)
             $0.height.equalTo(buttonHeigh)
         }
-
     }
     
-    func animate(line: UIView) {
+    private func attribute() {
+        view.backgroundColor = .white
+        
+        lazy var textFields = [firstTextField, secondTextField, thirdTextField]
+        let placeholders = ["아이디", "비밀번호", "비밀번호 확인"]
+        
+        var index = 0
+        for textField in textFields {
+            textField.attributedPlaceholder = NSAttributedString(string: placeholders[index],
+                                                                 attributes: attributes)
+            textField.delegate = self
+            index += 1
+        }
+    }
+    
+    private func animate(line: UIView) {
         line.alpha = 0.3
         UIView.animate(withDuration: 1) {
             line.alpha = 1
         }
     }
     @objc func nextPageButtonDidTap(_ sender: Any) {
-            self.navigationController?.pushViewController(InputTelViewController(), animated: true)
-        }
+        self.navigationController?.pushViewController(InputTelViewController(), animated: true)
+    }
 }
 
 extension InputIdPasswordViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         switch textField {
-        case firstTextField: animate(line: nicknameLine)
-            nicknameLine.backgroundColor = .pointBase
+        case firstTextField: animate(line: idLine)
+            idLine.backgroundColor = .pointBase
         case secondTextField: animate(line: passwordLine)
             passwordLine.backgroundColor = .pointBase
+        case thirdTextField: animate(line: checkPasswordLine)
+            checkPasswordLine.backgroundColor = .pointBase
         default: return
         }
     }
