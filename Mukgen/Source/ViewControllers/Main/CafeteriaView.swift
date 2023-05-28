04beb53dd31e3fcfa938c34999d5852abeee37de
junha <goodjunha@gmail.com>
2023-05-28 21:46:24 +0900

    🐛:: #34 isPagingEnabled 이상한거 고침

--- Mukgen/Source/ViewControllers/Main/CafeteriaView.swift
+++ Mukgen/Source/ViewControllers/Main/CafeteriaView.swift
@@ -1,10 +1,3 @@
-//
-//  CafeteriaView.swift
-//  Mukgen
-//
-//  Created by 이은호 on 2023/05/19.
-//
-
 import UIKit
 import SnapKit
 import Then
@@ -12,35 +5,76 @@ import Then
 class CafeteriaView: UIView {
     private final var controller: UIViewController
     
+    private enum Const {
+        static let itemSize = CGSize(width: 353, height: 220)
+        static let itemSpacing = 24.0
+        
+        static var insetX: CGFloat {
+            (UIScreen.main.bounds.width - Self.itemSize.width) / 2.0
+        }
+        static var collectionViewContentInset: UIEdgeInsets {
+            UIEdgeInsets(top: 0, left: Self.insetX, bottom: 0, right: Self.insetX)
+        }
+    }
+    
     private lazy var todayCafeteriaTitle = UILabel().then {
         $0.font = .systemFont(ofSize: 20.0, weight: .semibold)
         $0.text = "오늘의 급식"
         $0.textColor = .black
     }
     
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        collectionView.isPagingEnabled = true
-        collectionView.backgroundColor = .white
-        
-        collectionView.showsHorizontalScrollIndicator = false
-        
-        collectionView.register(CafeteriaCollecionViewCell.self, forCellWithReuseIdentifier: "CafeteriaCollecionViewCell")
-        
-        return collectionView
-    }()
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .horizontal
+        $0.itemSize = Const.itemSize
+        $0.minimumLineSpacing = Const.itemSpacing
+        $0.minimumInteritemSpacing = 0
+    }
+    
+    private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.collectionViewFlowLayout).then {
+        $0.isScrollEnabled = true
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = true
+        $0.backgroundColor = .clear
+        $0.clipsToBounds = true
+        $0.register(CafeteriaCollecionViewCell.self, forCellWithReuseIdentifier: "CafeteriaCollecionViewCell")
+        $0.isPagingEnabled = false
+        $0.contentInsetAdjustmentBehavior = .never
+        $0.contentInset = Const.collectionViewContentInset
+        $0.decelerationRate = .fast
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private var items = [1,2,3]
+    private var previousIndex: Int?
     
     init(frame: CGRect, viewController: UIViewController) {
         controller = viewController
         super.init(frame: frame)
-        
         layout()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func layout() {
+        [
+            todayCafeteriaTitle,
+            collectionView
+        ].forEach { addSubview($0) }
+
+        todayCafeteriaTitle.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(29)
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview().offset(20)
+        }
+
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(todayCafeteriaTitle.snp.bottom).offset(25.0)
+            $0.height.equalTo(240)
+            $0.bottom.equalToSuperview()
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -49,76 +83,55 @@ class CafeteriaView: UIView {
 }
 
 extension CafeteriaView: UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CafeteriaCollecionViewCell", for: indexPath) as? CafeteriaCollecionViewCell
-        
-        cell?.layout()
-        
-        cell?.layer.cornerRadius = 15.0
-        cell?.backgroundColor = .primaryLight3
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CafeteriaCollecionViewCell", for: indexPath) as! CafeteriaCollecionViewCell
+        cell.layout()
+        cell.backgroundColor = .white
+        cell.layer.cornerRadius = 10.0
+        cell.backgroundColor = .primaryLight3
+
         if indexPath.row == 1 {
-            cell?.breakfastImage.image = UIImage(named: "Lunch")
-            cell?.breakfastText.text = "점심"
-            
+            cell.breakfastImage.image = UIImage(named: "Lunch")
+            cell.breakfastText.text = "점심"
         }
-        
         if indexPath.row == 2 {
-            cell?.breakfastImage.image = UIImage(named: "Night")
-            cell?.breakfastText.text = "저녁"
+            cell.breakfastImage.image = UIImage(named: "Night")
+            cell.breakfastText.text = "저녁"
         }
         
-        
-        return cell ?? UICollectionViewCell()
+        return cell
     }
 }
 
-extension CafeteriaView: UICollectionViewDelegateFlowLayout{
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width:  (collectionView.frame.width) - 40.0, height: 220)
+extension CafeteriaView: UICollectionViewDelegateFlowLayout {
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let scrolledOffsetX = targetContentOffset.pointee.x + scrollView.contentInset.left
+        let cellWidth = Const.itemSize.width + Const.itemSpacing
+        let index = round(scrolledOffsetX / cellWidth)
+        targetContentOffset.pointee = CGPoint(x: index * cellWidth - scrollView.contentInset.left, y: scrollView.contentInset.top)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 20.0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 8.0
-    }
-}
-
-extension CafeteriaView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("\(indexPath.row) 클릭")
-    }
-}
-
-private extension CafeteriaView {
-    
-    func layout() {
-        [
-            todayCafeteriaTitle,
-            collectionView
-        ].forEach { addSubview($0) }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrolledOffset = scrollView.contentOffset.x + scrollView.contentInset.left
+        let cellWidth = Const.itemSize.width + Const.itemSpacing
+        let index = Int(round(scrolledOffset / cellWidth))
         
-        todayCafeteriaTitle.snp.makeConstraints {
-            $0.width.equalToSuperview()
-            $0.height.equalTo(29)
-            $0.top.equalToSuperview()
-            $0.left.equalToSuperview().offset(20)
+        defer {
+            previousIndex = index
+            collectionView.reloadData()
         }
         
-        collectionView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview()
-            $0.top.equalTo(todayCafeteriaTitle.snp.bottom).offset(25.0)
-            $0.height.equalTo(240)
-            $0.bottom.equalToSuperview()
-        }
+        guard
+            let previousIndex = previousIndex,
+            previousIndex != index
+        else { return }
     }
 }
