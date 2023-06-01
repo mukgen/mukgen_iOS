b420db80ef4ced07f9b77177cc98b072170cfc3f
junha <goodjunha@gmail.com>
2023-06-01 09:28:16 +0900

    🐛:: tuist core 오류 해결

--- Mukgen/Modules/Core/Sources/Base/BasezNC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BasezNC.swift
-//  Core
-//
-//  Created by 박준하 on 2023/05/31.
-//
-
-import Foundation
---
+++ Mukgen/Modules/Core/Tests/Tests.swift
@@ -0,0 +1,8 @@
+//
+//  Tests.swift
+//  Core
+//
+//  Created by 박준하 on 2023/06/01.
+//
+
+import Foundation
