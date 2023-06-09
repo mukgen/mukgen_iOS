7f0d5d48b951c9c88998d956e6fd2764349be7b6
s <tylereunholee010203@gmail.com>
2023-06-09 21:32:37 +0900

    Auto stash before merge of "main" and "origin/main"

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
@@ -6,7 +6,11 @@ public class BoardMainViewcontroller: UIViewController {
     
     public var factory: ModuleFactoryInterface!
     
+<<<<<<< Updated upstream:Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
     public override func viewDidLoad() {
+=======
+    override func viewDidLoad() {
+>>>>>>> Stashed changes:Mukgen/Modules/Presentation/Sources/Board/BoardViewController.swift
         super.viewDidLoad()
         
         view.backgroundColor = .red
