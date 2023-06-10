c06a64760e880e4f7c2f4fe9d78cf8ef003eb49e
s <tylereunholee010203@gmail.com>
2023-06-10 21:46:07 +0900

    ✨ :: #32 급식 게시판 타이틀 추가
    급식 게시판 제목 만들었어요

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardTitle/BoardTitleView.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardTitle/BoardTitleView.swift
@@ -1,8 +1,78 @@
-//
-//  BoardTitleView.swift
-//  Presentation
-//
-//  Created by 이은호 on 2023/06/10.
-//
+import UIKit
+import Then
+import SnapKit
 
-import Foundation
+class BoardTitleView: UIView {
+    private final var controller: UIViewController
+    
+    private lazy var boardTitleCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isPagingEnabled = false
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        collectionView.register(BoardTitleCell.self, forCellWithReuseIdentifier: BoardTitleCell.id)
+        
+        return collectionView
+    }()
+    
+    init(frame: CGRect ,viewController: UIViewController) {
+        self.controller = viewController
+        super.init(frame: frame)
+        boardTitleCollectionView.delegate = self
+        boardTitleCollectionView.dataSource = self
+        
+        layout()
+        boardTitleCollectionView.reloadData()
+    }
+    
+    func layout() {
+        self.addSubview(boardTitleCollectionView)
+        
+        boardTitleCollectionView.snp.makeConstraints {
+            $0.height.equalTo(29.0)
+            $0.top.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BoardTitleView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 353.0, height: 29.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 20.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
+
+extension BoardTitleView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("asdf")
+    }
+}
+
+extension BoardTitleView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    //cell에 관련된 것을 정의합니다.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BoardTitleCell.id, for: indexPath) as! BoardTitleCell
+        cell.backView.backgroundColor = .white
+        return cell
+    }
+}
