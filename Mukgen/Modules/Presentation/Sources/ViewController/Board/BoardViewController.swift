306b499289337eeedde38d5f01fd8c47c1c115c2
s <tylereunholee010203@gmail.com>
2023-06-10 21:46:52 +0900

    ✨ :: #32 게시판 메인 컨드롤러
    컨트롤러에 스택뷰 추가함

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
@@ -1,18 +1,69 @@
-import UIKit
-import Then
-import SnapKit
 
-public class BoardMainViewcontroller: UIViewController {
+
+import UIKit
+import SnapKit
+import Then
+import MukgenKit
+import Core
+
+public class BoardMainViewController: BaseVC {
     
     public var factory: ModuleFactoryInterface!
     
-<<<<<<< Updated upstream:Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardViewController.swift
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var stackView = UIStackView().then { stackView in
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        let boardTitleView = BoardTitleView(frame: .zero, viewController: self)
+        let showFilterView =  ShowFilterView(frame: .zero, viewController: self)
+        
+        let spacingView = UIView()
+        spacingView.snp.makeConstraints {
+            $0.height.equalTo(61.0)
+        }
+
+        [
+            boardTitleView,
+            spacingView,
+            showFilterView,
+            
+        ].forEach { stackView.addArrangedSubview($0) }
+    }
+    
+   
+    
     public override func viewDidLoad() {
-=======
-    override func viewDidLoad() {
->>>>>>> Stashed changes:Mukgen/Modules/Presentation/Sources/Board/BoardViewController.swift
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setupLayout()
+        view.backgroundColor = .white
+    }
+}
+
+private extension BoardMainViewController {
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        
     }
 }
