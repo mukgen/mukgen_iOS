f4bc0960127abf3da07e6c654db719ade2e0dc73
s <tylereunholee010203@gmail.com>
2023-06-12 08:58:22 +0900

    ♻️ :: #32 필요없는 주석 삭제 및 사진 올림

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsCell.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsCell.swift
@@ -2,7 +2,7 @@
 //  BoardContentsCell.swift
 //  Presentation
 //
-//  Created by 이은호 on 2023/06/10.
+//  Created by 이은호 on 2023/06/12.
 //
 
 import Foundation
--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsView.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsView.swift
@@ -1,8 +1,8 @@
 //
-//  BoardContents.swift
+//  BoardContentsView.swift
 //  Presentation
 //
-//  Created by 이은호 on 2023/06/10.
+//  Created by 이은호 on 2023/06/12.
 //
 
 import Foundation
