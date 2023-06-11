9933ebf9f78084a715f087063d5a862dd14989a6
s <tylereunholee010203@gmail.com>
2023-06-12 08:58:54 +0900

    ✨ :: #32 게시판
    게시판 만들고 있ㄷ어요

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsCell.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/BoardContents/BoardContentsCell.swift
@@ -1,8 +1,104 @@
-//
-//  BoardContentsCell.swift
-//  Presentation
-//
-//  Created by 이은호 on 2023/06/12.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+class BoardContentsCell: UICollectionViewCell {
+    
+    static let id = "customCell"
+    
+    var boardText = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+        $0.text = "제주도 가고싶다"
+        $0.textColor = .black
+    }
+    
+    var hotImage = UIImageView().then {
+        $0.image = PresentationAsset.Images.hot.image
+    }
+
+    public var backView = UIImageView().then {
+        $0.backgroundColor = .red
+    }
+    
+    private var chatImage = UIImageView().then {
+        $0.image = PresentationAsset.Images.chat.image
+    }
+    
+    private var viewImage = UIImageView().then {
+        $0.image = PresentationAsset.Images.eye.image
+    }
+    
+    private var chatCount = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .regular)
+        $0.text = "24"
+        $0.textColor = PresentationAsset.Colors.primaryLight2.color
+    }
+    
+    private var viewCount = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .regular)
+        $0.text = "16"
+        $0.textColor = PresentationAsset.Colors.primaryLight2.color
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(backView)
+        backView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.addSubview(hotImage)
+        hotImage.snp.makeConstraints {
+            $0.width.equalTo(32)
+            $0.height.equalTo(14)
+            $0.top.equalToSuperview().offset(18.5)
+            $0.bottom.equalToSuperview().inset(18.5)
+            $0.left.equalToSuperview().offset(24)
+        }
+        
+        contentView.addSubview(boardText)
+        boardText.snp.makeConstraints {
+            $0.left.equalTo(hotImage.snp.right).offset(6)
+            $0.top.equalToSuperview().offset(16)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+        
+        contentView.addSubview(chatImage)
+        chatImage.snp.makeConstraints {
+            $0.right.equalToSuperview().inset(100)
+            $0.top.equalToSuperview().offset(16.5)
+            $0.bottom.equalToSuperview().inset(16.5)
+        }
+        
+        contentView.addSubview(viewImage)
+        viewImage.snp.makeConstraints {
+            $0.right.equalToSuperview().inset(46)
+            $0.top.equalToSuperview().offset(16.5)
+            $0.bottom.equalToSuperview().inset(16.5)
+        }
+        
+        contentView.addSubview(chatCount)
+        chatCount.snp.makeConstraints {
+            $0.left.equalTo(chatImage.snp.right).offset(4)
+            $0.top.equalToSuperview().offset(16)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+
+        contentView.addSubview(viewCount)
+        viewCount.snp.makeConstraints {
+            $0.left.equalTo(viewImage.snp.right).offset(4)
+            $0.top.equalToSuperview().offset(16)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+        
+        
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
