fdac092c035452ca5f0d9695ff54d48e042cf9a7
s <tylereunholee010203@gmail.com>
2023-06-17 16:02:08 +0900

    ♻️ :: #51 리펙토링

--- Mukgen/Modules/Presentation/Sources/ViewController/Board/ShowFilterView/ShowFilterView.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Board/ShowFilterView/ShowFilterView.swift
@@ -41,15 +41,19 @@ class ShowFilterView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+//    @objc func toggleDidTap(_ sender: Any) {
+//
+//    }
 }
 
 extension ShowFilterView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: 60, height: 35.0)
+        CGSize(width: 60.0, height: 35.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        UIEdgeInsets(top: 0.0, left: 10.0, bottom: 0.0, right: 0.0)
+        UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 0.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -60,7 +64,7 @@ extension ShowFilterView: UICollectionViewDelegateFlowLayout {
 extension ShowFilterView: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("asdf")
+        
     }
 }
 
@@ -72,10 +76,21 @@ extension ShowFilterView: UICollectionViewDataSource {
     //cell에 관련된 것을 정의합니다.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ShowFilterCell.id, for: indexPath) as! ShowFilterCell
-        cell.backView.backgroundColor = .white
-        cell.backView.layer.borderWidth = 1
-        cell.backView.layer.borderColor = PresentationAsset.Colors.primaryLight2.color.cgColor
-        cell.backView.layer.cornerRadius = 100.0
+        cell.filterBackView.backgroundColor = .white
+        cell.filterBackView.layer.borderWidth = 1
+        cell.filterBackView.layer.borderColor = PresentationAsset.Colors.primaryLight2.color.cgColor
+        cell.filterBackView.layer.cornerRadius = 17.5
+        
+        switch indexPath.row {
+        case 0:
+            break
+        case 1 :
+            cell.filterToggle.setTitle("일간", for: .normal)
+        default:
+            cell.filterToggle.setTitle("주간", for: .normal)
+        }
         return cell
     }
 }
+
+
