e89ba5c1bb53d6575a5602b3d256149ab7b3e57a
s <tylereunholee010203@gmail.com>
2023-06-29 10:35:08 +0900

    ✨ :: #52 급식 건의 메인 컨트롤러
    스택뷰 스크롤뷰로 구성

--- Mukgen/Modules/Presentation/Sources/ViewController/Tendinous/TendinousViewController.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Tendinous/TendinousViewController.swift
@@ -1,17 +1,72 @@
-//
-//  TendinousViewController.swift
-//  Presentation
-//
-//  Created by 박준하 on 2023/06/09.
-//
-
 import UIKit
+import SnapKit
+import Then
+import MukgenKit
+import Core
 
-class TendinousViewController: UIViewController {
+public class TendinousViewController: BaseVC {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    public var factory: ModuleFactoryInterface!
+    
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var stackView = UIStackView().then { stackView in
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
         
-        view.backgroundColor = .yellow
+        let tendinousView = TendinousView(frame: .zero, viewController: self)
+        
+        let spacingView1 = UIView()
+        spacingView1.snp.makeConstraints {
+            $0.height.equalTo(8.0)
+        }
+        
+        let spacingView2 = UIView()
+        spacingView2.snp.makeConstraints {
+            $0.height.equalTo(64)
+        }
+
+        [
+            spacingView1,
+            tendinousView,
+        ].forEach { stackView.addArrangedSubview($0) }
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.isScrollEnabled = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+   
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        setupLayout()
+        view.backgroundColor = .white
+    }
+}
+
+private extension TendinousViewController {
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        
     }
 }
