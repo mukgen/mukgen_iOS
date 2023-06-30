c69dfeb0ca734ccbea87f1ff458a56120bc750a8
s <tylereunholee010203@gmail.com>
2023-06-30 20:50:41 +0900

    ✨ :: #55 collectionViewCell
    콜렉션뷰 셀 입니다

--- Mukgen/Modules/Presentation/Sources/ViewController/Delivery/DeliveryContents/DeliveryContentsCell.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/DeliveryContents/DeliveryContentsCell.swift
@@ -15,13 +15,66 @@ class DeliveryContentsCell: UICollectionViewCell {
     
     var contents = UILabel().then {
         $0.font = .systemFont(ofSize: 12.11, weight: .regular)
-        $0.text = "PM이 맛있습니당!!"
+        $0.text = "제주한라봉"
         $0.textColor = .black
+    }
+    
+    var contentsTime = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.text = "제주도 | 오후 06:00"
+        $0.textColor = PresentationAsset.Colors.primaryLight1.color
+    }
+    
+    var countPeople = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.text = "7 / 8"
+        $0.textColor = PresentationAsset.Colors.primaryDark1.color
+    }
+    
+    var perticipatePerson1 = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.text = "이은호"
+        $0.textColor = PresentationAsset.Colors.primaryDark1.color
+    }
+    
+    var perticipatePerson2 = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.text = "햄스터"
+        $0.textColor = PresentationAsset.Colors.primaryDark1.color
+    }
+
+    var perticipatePerson3 = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.text = "이태영"
+        $0.textColor = PresentationAsset.Colors.primaryDark1.color
+    }
+    
+    public var perticipateIn = UIButton().then {
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        $0.setTitle("참여하기", for: .normal)
+        $0.tintColor = .white
+        $0.backgroundColor = PresentationAsset.Colors.pointBase.color
+        $0.layer.cornerRadius = 10.0
     }
     
     public var profileImage = UIImageView().then {
         $0.image = PresentationAsset.Images.testProfile.image
     }
+    
+    public var profileImage1 = UIImageView().then {
+        $0.image = PresentationAsset.Images.testProfile1.image
+    }
+    
+    public var profileImage2 = UIImageView().then {
+        $0.image = PresentationAsset.Images.testProfile2.image
+    }
+    
+    public var profileImage3 = UIImageView().then {
+        $0.image = PresentationAsset.Images.testProfile3.image
+    }
+
+
+
 
     var heartBackView = UIView().then {
         $0.layer.cornerRadius = 8.07
@@ -81,54 +134,69 @@ class DeliveryContentsCell: UICollectionViewCell {
             $0.left.equalTo(backView.snp.left).offset(91.0)
             $0.top.equalTo(writer.snp.bottom).offset(4.0)
         }
-//
-//        contentView.addSubview(heartBackView)
-//        heartBackView.snp.makeConstraints {
-//            $0.width.equalTo(40)
-//            $0.height.equalTo(20)
-//            $0.bottom.equalToSuperview().inset(12.0)
-//            $0.left.equalToSuperview().offset(75.5)
-//        }
         
-//        contentView.addSubview(heartImage)
-//        heartImage.snp.makeConstraints {
-//            $0.left.equalTo(heartBackView.snp.left).offset(8.07)
-//            $0.bottom.equalTo(heartBackView.snp.bottom).inset(4.45)
-//        }
-//
-//        contentView.addSubview(heartCount)
-//        heartCount.snp.makeConstraints {
-//            $0.left.equalTo(heartImage.snp.right).offset(4.04)
-//            $0.top.equalTo(heartBackView.snp.top).offset(2.5)
-//        }
+        contentView.addSubview(contentsTime)
+        contentsTime.snp.makeConstraints {
+            $0.left.equalTo(backView.snp.left).offset(91.0)
+            $0.top.equalTo(contents.snp.bottom).offset(4.0)
+        }
         
-//        contentView.addSubview(noHeartBackView)
-//        noHeartBackView.snp.makeConstraints {
-//            $0.left.equalTo(heartBackView.snp.right).offset(4.0)
-//            $0.width.equalTo(40)
-//            $0.height.equalTo(20)
-//            $0.bottom.equalToSuperview().inset(12.0)
-//        }
-        
-//        contentView.addSubview(noHeartImage)
-//        noHeartImage.snp.makeConstraints {
-//            $0.left.equalTo(noHeartBackView.snp.left).offset(8.07)
-//            $0.bottom.equalTo(noHeartBackView.snp.bottom).inset(4.45)
-//        }
-//
-//        contentView.addSubview(noHeartCount)
-//        noHeartCount.snp.makeConstraints {
-//            $0.left.equalTo(noHeartImage.snp.right).offset(4.04)
-//            $0.top.equalTo(noHeartBackView.snp.top).offset(2.5)
-//        }
+        contentView.addSubview(countPeople)
+        countPeople.snp.makeConstraints {
+            $0.right.equalTo(backView.snp.right).inset(15.0)
+            $0.top.equalTo(backView.snp.top).offset(20.0)
+        }
         
         contentView.addSubview(profileImage)
         profileImage.snp.makeConstraints {
             $0.top.equalTo(backView.snp.top).offset(15)
             $0.left.equalTo(backView.snp.left).offset(16)
-            $0.bottom.equalTo(backView.snp.bottom).inset(15)
+          
         }
         
+        contentView.addSubview(profileImage1)
+        profileImage1.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).inset(90)
+            $0.left.equalTo(backView.snp.left).offset(13)
+        }
+        
+        contentView.addSubview(profileImage2)
+        profileImage2.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).offset(90)
+            $0.left.equalTo(profileImage1.snp.right).offset(12)
+        }
+    
+        contentView.addSubview(profileImage3)
+        profileImage3.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).offset(90)
+            $0.left.equalTo(profileImage2.snp.right).offset(12)
+        }
+        
+        contentView.addSubview(perticipatePerson1)
+        perticipatePerson1.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).offset(122)
+            $0.left.equalTo(backView.snp.left).offset(12)
+        }
+        
+        contentView.addSubview(perticipatePerson2)
+        perticipatePerson2.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).offset(122)
+            $0.left.equalTo(perticipatePerson1.snp.right).offset(10)
+        }
+        
+        contentView.addSubview(perticipatePerson3)
+        perticipatePerson3.snp.makeConstraints {
+            $0.top.equalTo(backView.snp.top).offset(122)
+            $0.left.equalTo(perticipatePerson2.snp.right).offset(10)
+        }
+        
+        contentView.addSubview(perticipateIn)
+        perticipateIn.snp.makeConstraints {
+            $0.width.equalTo(333.0)
+            $0.height.equalTo(39.0)
+            $0.bottom.equalTo(backView.snp.bottom).inset(10.0)
+            $0.left.equalTo(backView.snp.left).offset(10.0)
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
