8bdfc53d76e3dc5dfcc7e757bae25b9695238e33
s <tylereunholee010203@gmail.com>
2023-06-30 16:35:40 +0900

    ✨ :: #55 배달파티 메인 게시판
    collectionView 사용

---
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/DeliveryContents/DeliveryContentsView.swift
@@ -0,0 +1,92 @@
+import UIKit
+import Then
+import SnapKit
+
+    public class DeliveryContentsView: UIView {
+    
+    private final var controller: UIViewController
+
+    private lazy var deliveryContentsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = true
+        collectionView.backgroundColor = .clear
+        collectionView.register(DeliveryContentsCell.self, forCellWithReuseIdentifier: DeliveryContentsCell.id)
+        return collectionView
+    }()
+    
+    init(frame: CGRect ,viewController: UIViewController) {
+        self.controller = viewController
+        super.init(frame: frame)
+        deliveryContentsCollectionView.delegate = self
+        deliveryContentsCollectionView.dataSource = self
+        
+        layout()
+        deliveryContentsCollectionView.reloadData()
+    }
+    
+    public var plusButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "plus"), for: .normal)
+        $0.backgroundColor = PresentationAsset.Colors.pointBase.color
+        $0.layer.cornerRadius = 30
+    }
+    
+    func layout() {
+        self.addSubview(deliveryContentsCollectionView)
+        self.addSubview(plusButton)
+        
+        deliveryContentsCollectionView.snp.makeConstraints {
+            $0.height.equalTo(640.0)
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        plusButton.snp.makeConstraints {
+            $0.width.height.equalTo(60)
+            $0.bottom.equalToSuperview().inset(20.0)
+            $0.right.equalToSuperview().inset(20.0)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DeliveryContentsView: UICollectionViewDelegateFlowLayout {
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 353.0, height: 90)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 20.0)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
+
+extension DeliveryContentsView: UICollectionViewDelegate {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("asdf")
+    }
+}
+
+extension DeliveryContentsView: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    //cell에 관련된 것을 정의합니다.
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let deliveryContentsCell = collectionView.dequeueReusableCell(withReuseIdentifier: DeliveryContentsCell.id, for: indexPath) as! DeliveryContentsCell
+        deliveryContentsCell.backView.backgroundColor = PresentationAsset.Colors.primaryLight3.color
+        deliveryContentsCell.backView.layer.cornerRadius = 10.0
+        return deliveryContentsCell
+    }
+}
