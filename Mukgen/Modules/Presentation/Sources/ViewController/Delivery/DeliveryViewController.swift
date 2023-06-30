b39904c82d5cd02b600f4dd0bc516460b4e152d7
s <tylereunholee010203@gmail.com>
2023-06-30 16:35:10 +0900

    ✨ :: #55 배달파티 VC
    스크롤뷰 스택뷰 사용

--- Mukgen/Modules/Presentation/Sources/ViewController/Delivery/DeliveryViewController.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/DeliveryViewController.swift
@@ -1,22 +1,73 @@
-//
-//  DeliveryViewController.swift
-//  Presentation
-//
-//  Created by 박준하 on 2023/06/09.
-//
-
 import UIKit
+import SnapKit
+import Then
+import MukgenKit
+import Core
 
-class DeliveryViewController: UIViewController {
+public class DeliveryViewController: BaseVC {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    public var factory: ModuleFactoryInterface!
+    
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var stackView = UIStackView().then { stackView in
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
         
-        if #available(iOS 15.0, *) {
-            view.backgroundColor = .systemCyan
-        } else {
-            // Fallback on earlier versions
-            view.backgroundColor = .gray
+        let deliveryContentsView = DeliveryContentsView(frame: .zero, viewController: self)
+        
+        let spacingView1 = UIView()
+        spacingView1.snp.makeConstraints {
+            $0.height.equalTo(8.0)
         }
+        
+        let spacingView2 = UIView()
+        spacingView2.snp.makeConstraints {
+            $0.height.equalTo(64)
+        }
+
+        [
+            spacingView1,
+            deliveryContentsView,
+        ].forEach { stackView.addArrangedSubview($0) }
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.isScrollEnabled = false
+        scrollView.showsHorizontalScrollIndicator = false
+    }
+    
+   
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        setupLayout()
+        view.backgroundColor = .white
     }
 }
+
+private extension DeliveryViewController {
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        
+    }
+}
+
