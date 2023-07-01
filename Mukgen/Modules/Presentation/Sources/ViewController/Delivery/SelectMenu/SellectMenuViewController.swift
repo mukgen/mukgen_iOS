a12728e895b6b8c25fdf493c741e09af0757ffba
s <tylereunholee010203@gmail.com>
2023-07-01 17:42:23 +0900

    Auto stash before checking out "origin/main"

---
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SelectMenu/SellectMenuViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+class SellectMenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
