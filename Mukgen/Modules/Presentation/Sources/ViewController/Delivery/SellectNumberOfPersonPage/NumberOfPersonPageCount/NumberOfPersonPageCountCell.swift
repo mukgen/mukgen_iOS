d26875588115f96692e2d64be4f1c7145b26c064
s <tylereunholee010203@gmail.com>
2023-07-02 19:06:53 +0900

    ✨ :: #57 페이지 카운트 collectionView

---
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SellectNumberOfPersonPage/NumberOfPersonPageCount/NumberOfPersonPageCountCell.swift
@@ -0,0 +1,40 @@
+import UIKit
+import SnapKit
+import Then
+
+class NumberOfPersonPageCountCell: UICollectionViewCell {
+    
+    static let id = "SellectPageCell"
+    
+    var countPage = UILabel().then {
+        $0.text = "2 / 4"
+        $0.textColor = PresentationAsset.Colors.primaryLight2.color
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+    }
+
+    public var backView = UIImageView().then {
+        $0.backgroundColor = .red
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(backView)
+        backView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.addSubview(countPage)
+        countPage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(0)
+            $0.right.equalToSuperview().inset(0)
+        }
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
