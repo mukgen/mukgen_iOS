9ac02e1405af3bbf3b297ff13e4c3701d3b05422
s <tylereunholee010203@gmail.com>
2023-07-14 22:01:12 +0900

    ⚡️ :: #65 BaseVC에 키보드 내림 추가
    화면 터치시 키보드 내리는 것 구현했어요

--- Mukgen/Modules/Core/Sources/Base/BaseVC.swift
+++ Mukgen/Modules/Core/Sources/Base/BaseVC.swift
@@ -7,11 +7,20 @@ open class BaseVC: UIViewController {
     
     open override func viewDidLoad() {
         super.viewDidLoad()
-        
         layout()
         attribute()
+        hideKeyboardWhenTappedArround()
     }
     
+    open func hideKeyboardWhenTappedArround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard) )
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
     open func layout() {}
     open func attribute() {}
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
