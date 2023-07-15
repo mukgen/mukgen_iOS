7a780b2c4a691f0f96a8f48c4814742af589ade0
s <tylereunholee010203@gmail.com>
2023-07-15 20:21:36 +0900

    ♻️ :: #65 UIPickerView성공
    PickerView 작동 되게 수정했습니다

--- Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SellectNumberOfPersonPage/ScrollNumberOfPersonPage/ScrollNumberOfPersonPageView.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SellectNumberOfPersonPage/ScrollNumberOfPersonPage/ScrollNumberOfPersonPageView.swift
@@ -3,28 +3,27 @@ import Then
 import SnapKit
 
 class ScrollNumberOfPersonPageView: UIView {
-    private let data = ["1", "2", "3", "4", "5"]
+    private let data = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     
     
     private final var controller: UIViewController
     
     private lazy var pickerView: UIPickerView = {
-            let pickerView = UIPickerView()
+        let pickerView = UIPickerView()
         pickerView.backgroundColor = .clear
         pickerView.showsLargeContentViewer = true
-        pickerView.showsSelectionIndicator = true
-            
-            // UIPickerView의 데이터 및 델리게이트 설정
         pickerView.delegate = self
         pickerView.dataSource = self
             
-            return pickerView
-        }()
+        return pickerView
+    }()
+    
+    private let feedbackGenerator = UISelectionFeedbackGenerator()
         
         init(frame: CGRect, viewController: UIViewController) {
             self.controller = viewController
             super.init(frame: frame)
-            
+                
             layout()
             pickerView.reloadAllComponents()
         }
@@ -33,7 +32,8 @@ class ScrollNumberOfPersonPageView: UIView {
             self.addSubview(pickerView)
             
             pickerView.snp.makeConstraints {
-                $0.height.equalTo(216.0) // 필요한 높이를 지정
+                $0.height.equalTo(240.0)
+                $0.bottom.top.equalToSuperview()
                 $0.top.leading.trailing.equalToSuperview()
             }
         }
@@ -44,23 +44,47 @@ class ScrollNumberOfPersonPageView: UIView {
 }
 
 extension ScrollNumberOfPersonPageView: UIPickerViewDelegate, UIPickerViewDataSource {
-    // UIPickerView의 컴포넌트 수
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    // UIPickerView의 행 수
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return data.count // 데이터 배열의 항목 수를 반환하도록 수정해야합니다.
+        return data.count
     }
     
-    // UIPickerView의 각 행에 대한 표시 제목
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return data[row] // 데이터 배열에서 적절한 항목을 반환하도록 수정해야합니다.
+        return data[row]
     }
     
-    // 사용자가 UIPickerView에서 행을 선택했을 때 호출됨
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // 선택된 행 처리
+        print("row: \(row)")
+        print("value: \(data[row])")
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        // 원하는 행 높이를 반환합니다.
+        return 56.0 // 예시로 50 포인트로 설정
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titles = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]// 행 제목 배열
+        
+        // 행에 대한 폰트 및 속성 설정
+        let attributes: [NSAttributedString.Key: Any]
+        
+        switch row {
+        case 0:
+            attributes = [
+                .font: UIFont.systemFont(ofSize: 20.0, weight: .semibold)
+            ]
+        default:
+            attributes = [
+                .font: UIFont.systemFont(ofSize: 16.0, weight: .semibold)
+            ]
+        }
+        
+        let title = titles[row]
+        
+        return NSAttributedString(string: title, attributes: attributes)
     }
 }
