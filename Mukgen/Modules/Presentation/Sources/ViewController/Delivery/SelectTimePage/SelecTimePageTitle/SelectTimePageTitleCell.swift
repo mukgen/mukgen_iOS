049788d266e942e7424cd5cf5d0a93e24edd7fa0
s <tylereunholee010203@gmail.com>
2023-07-15 23:53:25 +0900

    ✨ :: #69 제목
    제목

--- Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SelectTimePage/SelecTimePageTitle/SelectTimePageTitleCell.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/Delivery/SelectTimePage/SelecTimePageTitle/SelectTimePageTitleCell.swift
@@ -1,8 +1,42 @@
-//
-//  SelectTimePageTitleCell.swift
-//  Presentation
-//
-//  Created by 이은호 on 2023/07/15.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+class SelectTimePageTitleCell: UICollectionViewCell {
+    
+    static let id = "SelectTimePageTitleCell"
+    
+    var countPage = UILabel().then {
+        $0.numberOfLines = 2
+        $0.text = "몇시에\n만나실 건가요?"
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 24, weight: .semibold)
+    }
+
+    public var backView = UIImageView().then {
+        $0.backgroundColor = .red
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(backView)
+        backView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.addSubview(countPage)
+        countPage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(0)
+            $0.left.equalToSuperview().offset(0)
+        }
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
