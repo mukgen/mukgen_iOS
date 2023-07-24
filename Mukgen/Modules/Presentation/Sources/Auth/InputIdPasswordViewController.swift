6a338cec089a1dcc86c1aabbbfdd5a8285c67578
junha <goodjunha@gmail.com>
2023-07-24 18:16:06 +0900

    ♻️:: #72 InputIdPasswordViewController 키보드, 버튼 제약 조건 걸기

--- Mukgen/Modules/Presentation/Sources/Auth/InputIdPasswordViewController.swift
+++ Mukgen/Modules/Presentation/Sources/Auth/InputIdPasswordViewController.swift
@@ -148,6 +148,41 @@ public class InputIdPasswordViewController: BaseVC {
         }
         
         nextPageButton.addTarget(self, action: #selector(nextPageButtonDidTap), for: .touchUpInside)
+        
+        setupTextFieldObservers()
+        setupKeyboardObservers()
+        configureTextFields()
+    }
+    
+    private func configureTextFields() {
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+        thirdTextField.delegate = self
+        
+        firstTextField.tag = 1
+        secondTextField.tag = 2
+        thirdTextField.tag = 3
+        
+        firstTextField.addTarget(self, action: #selector(textFieldContentDidChange(_:)), for: .editingChanged)
+        secondTextField.addTarget(self, action: #selector(textFieldContentDidChange(_:)), for: .editingChanged)
+        thirdTextField.addTarget(self, action: #selector(textFieldContentDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func updateButtonColor() {
+        if firstTextField.text?.isEmpty == false && secondTextField.text?.isEmpty == false && thirdTextField.text?.isEmpty == false {
+            DispatchQueue.main.async {
+                self.nextPageButton.backgroundColor = MukgenKitAsset.Colors.primaryBase.color
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.nextPageButton.backgroundColor = MukgenKitAsset.Colors.primaryLight2.color
+            }
+        }
+    }
+
+    
+    private func setupTextFieldObservers() {
+        firstTextField.addTarget(self, action: #selector(textFieldContentDidChange(_:)), for: .editingChanged)
     }
     
     private func animate(line: UIView) {
@@ -155,6 +190,36 @@ public class InputIdPasswordViewController: BaseVC {
         UIView.animate(withDuration: 1) {
             line.alpha = 1
         }
+    }
+    
+    private func setupKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+
+            UIView.animate(withDuration: 0.3) {
+                self.nextPageButton.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight + 10) //만약 디자인에서 키보드와 거리가 표기되어있다면 keyoardHeight에 붙는 +를 바꾸면 붙는 간격을 바꿀 수 있습니다
+            }
+        }
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.nextPageButton.transform = .identity
+        }
+    }
+    
+    @objc private func textFieldContentDidChange(_ textField: UITextField) {
+        updateButtonColor()
     }
     
     @objc func nextPageButtonDidTap(_ sender: Any) {
@@ -174,4 +239,25 @@ extension InputIdPasswordViewController: UITextFieldDelegate {
         default: return
         }
     }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case firstTextField:
+            secondTextField.becomeFirstResponder()
+        case secondTextField:
+            thirdTextField.becomeFirstResponder()
+        case thirdTextField:
+            thirdTextField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+        idLine.backgroundColor = MukgenKitAsset.Colors.primaryLight2.color
+        passwordLine.backgroundColor = MukgenKitAsset.Colors.primaryLight2.color
+        checkPasswordLine.backgroundColor = MukgenKitAsset.Colors.primaryLight2.color
+   }
 }
