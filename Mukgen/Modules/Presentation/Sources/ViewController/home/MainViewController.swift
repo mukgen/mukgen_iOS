d4e61f973fff07251f5be6aa08d4bac40a1eb9f9
s <tylereunholee010203@gmail.com>
2023-07-26 20:20:04 +0900

    ♻️ :: #71 JSON 데이터 파씽 도전중
    도와주세요 ㅠ

--- Mukgen/Modules/Presentation/Sources/ViewController/home/MainViewController.swift
+++ Mukgen/Modules/Presentation/Sources/ViewController/home/MainViewController.swift
@@ -74,8 +74,45 @@ private extension MainViewController {
     }
 }
 
-func getMeal() {
+//func getMeal() {
+//
+//    struct TodayMeal: Codable {
+//        let riceType: String
+//        let item: String
+//        let riceId: Int
+//    }
+//
+//    var components = URLComponents(string: "https://www.mukgen.info")
+//    components?.path = "/meal/today"
+//
+//    guard let url = components?.url else {
+//        fatalError("Invalid URL components")
+//    }
+//
+//    var request = URLRequest(url: url)
+//    request.httpMethod = "GET"
+//
+//    request.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0eWxlcjA5MjIiLCJpYXQiOjE2OTAzNjk0MTksImV4cCI6MTY5MDM3MTIxOX0.Oh01WK3_wYaPa4YdYCkIAdOSueqh4JXzb6xct7f56yg",
+//                     forHTTPHeaderField: "Authorization")
+//
+//    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//        if let error = error {
+//            print("Error: \(error)")
+//        } else if let data = data {
+//            do {
+//                print(data)
+////                let decoder = JSONDecoder()
+////                let resultData = try decoder.decode(TodayMeal.self, from: data)
+////                print("riceType: \(resultData.riceType), item: \(resultData.item), riceId: \(resultData.riceId)")
+//            } catch {
+//                print("Error: \(error)")
+//            }
+//        }
+//    }
+//    task.resume()
+//}
 
+func getMeal() {
     var jsonEncoder = JSONEncoder()
 
     var components = URLComponents(string: "https://www.mukgen.info")
@@ -88,126 +125,21 @@ func getMeal() {
     var requeset = URLRequest(url: url)
     requeset.httpMethod = "GET"
 
-    requeset.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhenhjdjE3NjgiLCJpYXQiOjE2OTAxNjMxNDksImV4cCI6MTY5MDE2NDk0OX0.1gSNCdKJtZjGMbdRkTFLj4agQZ_-rf8_x8JdXklHkm4", forHTTPHeaderField: "Authorization")
-
-//    var resultData: Any
+    requeset.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0eWxlcjA5MjIiLCJpYXQiOjE2OTAzNjk0MTksImV4cCI6MTY5MDM3MTIxOX0.Oh01WK3_wYaPa4YdYCkIAdOSueqh4JXzb6xct7f56yg", forHTTPHeaderField: "Authorization")
 
     let task = URLSession.shared.dataTask(with: requeset) { data, response, error in
         if let error = error {
             print("Error: \(error)")
         } else if let data = data {
             do {
-                let resultData = String(data: data, encoding: .utf8) ?? ""
-                print(resultData)
+                let jsonDataString = String(data: data, encoding: .utf8) ?? ""
+                print(jsonDataString)
+                
+                } catch {
+                print("Error during JSON serialization: \(error)")
             }
         }
     }
-//    var jsonData: Any
-//    jsonData = resultData
-//
-//    do {
-//        if let jsonArray = try JSONSerialization.jsonObject(with: jsonData, options: [])as? [[String: Any]] {
-//            for json in jsonArray {
-//                let item = json["item"]as? String
-//                print("\(item)")
-//            }
-//        }
-//
-//
-//        }
 
     task.resume()
 }
-
-//func getMeal() {
-//    var components = URLComponents(string: "https://www.mukgen.info")
-//    components?.path = "/meal/today"
-//
-//    guard let url = components?.url else {
-//        fatalError("Invalid URL components")
-//    }
-//
-//    var request = URLRequest(url: url)
-//    request.httpMethod = "GET"
-//
-//    request.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJoeXVuc3UxNzY4IiwiaWF0IjoxNjkwMTU1ODcwLCJleHAiOjE2OTAxNTc2NzB9.q_mnUfJYsiAam3q5P05gV3SxSVFntD9FjORRV6OViFY", forHTTPHeaderField: "Authorization")
-//
-//    let semaphore = DispatchSemaphore(value: 0)
-//
-//    URLSession.shared.dataTask(with: request) { (data, response, error) in
-//
-//        defer {
-//            semaphore.signal()
-//        }
-//
-//        if let error = error {
-//            print("Error: \(error)")
-//            return
-//        }
-//
-//        guard let data = data else {
-//            print("Error: no data received")
-//            return
-//        }
-//
-//        do {
-//            if let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
-//                for json in jsonArray {
-//                    if let item = json["responseList"] as? String {
-//                        print(item)
-//                    }
-//                }
-//            } else {
-//                print("Error converting data to JSON array")
-//            }
-//        } catch {
-//            print("Error during JSON serialization: \(error)")
-//        }
-//
-//    }.resume()
-//
-//    semaphore.wait()
-//}
-
-//getMeal()
-
-//func getMeal() {
-//    var jsonEncoder = JSONEncoder()
-//
-//    var components = URLComponents(string: "https://www.mukgen.info")
-//    components?.path = "/meal/today"
-//
-//    guard let url = components?.url else {
-//        fatalError("Invalid URL components")
-//    }
-//
-//    var requeset = URLRequest(url: url)
-//    requeset.httpMethod = "GET"
-//
-//    requeset.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJoeXVuc3UxNzY4IiwiaWF0IjoxNjkwMTU1ODcwLCJleHAiOjE2OTAxNTc2NzB9.q_mnUfJYsiAam3q5P05gV3SxSVFntD9FjORRV6OViFY", forHTTPHeaderField: "Authorization")
-//
-//    let task = URLSession.shared.dataTask(with: requeset) { data, response, error in
-//        if let error = error {
-//            print("Error: \(error)")
-//        } else if let data = data {
-//            do {
-//                let jsonDataString = String(data: data, encoding: .utf8) ?? ""
-//                print(jsonDataString)
-//
-//                if let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
-//
-//                    for json in jsonArray {
-//                        let riceType = json["riceType"] as? String
-//                        let item = json["item"] as? String
-//                        let riceId = json["riceId"] as? String
-//                        print(item)
-//                    }
-//                }
-//            } catch {
-//                print("Error during JSON serialization: \(error)")
-//            }
-//        }
-//    }
-//
-//    task.resume()
-//}
