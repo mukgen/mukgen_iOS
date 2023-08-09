0e30890472c22f3e89ded2ffe70753f60491bedd
junha <goodjunha@gmail.com>
2023-08-09 22:39:43 +0900

    🐛:: #83 에러 해결

--- Mukgen/Service/MealService/Sources/MukgenPickService/MukgenPickService.swift
+++ Mukgen/Service/MealService/Sources/MukgenPickService/MukgenPickService.swift
@@ -1,11 +1,15 @@
 import Foundation
 import UIKit
 
-public class MukgenPickService {
+open class MukgenPickService {
     public let baseURL = "https://www.mukgen.info"
     public let endpoint = "/mukgen-pick"
     public let token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0eWxlcjA5MjIiLCJpYXQiOjE2OTE1ODI4MjAsImV4cCI6MTY5MTU4NDYyMH0.CmTS7k8qar3x_KykuDAoJ4GFJGPkFgJozZ2csWNRaCk"
-    public func fetchRiceMenu(completion: @escaping (MukgenPickMenuResponse?) -> Void) {
+    
+    //에러에서는 init이 기본값이 internal이다 이기 떄문에 기본을 public으로 수정
+    public init() {}
+    
+    open func fetchRiceMenu(completion: @escaping (MukgenPickMenuResponse?) -> Void) {
         let url = URL(string: baseURL + endpoint)
         var request = URLRequest(url: url!)
         request.httpMethod = "GET"
