1c520330bdb14ecaa01ac7a0ef954c887bd83e4c
junha <goodjunha@gmail.com>
2023-08-15 13:17:56 +0900

    ✨:: 알럿 예시 코드

---
+++ Mukgen/Modules/Presentation/Sources/TextAlertViewController.swift
@@ -0,0 +1,77 @@
+//
+//  TextAlertViewController.swift
+//  Presentation
+//
+//  Created by 박준하 on 2023/08/15.
+//
+
+
+import UIKit
+import SnapKit
+import Then
+import MukgenKit
+import Core
+
+public class TextAlertViewController: UIViewController {
+    
+    public var factory: ModuleFactoryInterface!
+
+    private let showAlertButton = UIButton().then {
+        $0.setTitle("알림 표시", for: .normal)
+        $0.setTitleColor(.systemBlue, for: .normal)
+    }
+    
+    private let darkeningView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.12)
+        return view
+    }()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupViews() {
+        view.addSubview(showAlertButton)
+        showAlertButton.addTarget(self, action: #selector(onShowAlertButtonTapped), for: .touchUpInside)
+    }
+
+    private func setupConstraints() {
+        showAlertButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+
+    @objc private func onShowAlertButtonTapped() {
+        darkeningView.frame = view.bounds
+        darkeningView.alpha = 0
+        view.addSubview(darkeningView)
+        UIView.animate(withDuration: 0.3) {
+            self.darkeningView.alpha = 1
+        }
+
+        let customAlert = CustomAlertView(
+            labelText: "TaeYoung1234 님\n환영합니다!",
+            buttonLabelText: "확인",
+            buttonAction: { [weak self] in
+                print("알림 버튼이 눌렸습니다.")
+                self?.removeDarkeningView()
+            }
+        )
+        view.addSubview(customAlert)
+        customAlert.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func removeDarkeningView() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.darkeningView.alpha = 0
+        }, completion: { _ in
+            self.darkeningView.removeFromSuperview()
+        })
+    }
+}
