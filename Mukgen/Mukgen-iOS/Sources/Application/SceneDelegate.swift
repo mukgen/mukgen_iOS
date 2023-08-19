b1b7e6f87da501b09cff41c0da45c06fa58bfeb2
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-08-19 14:34:57 +0900

    ♻️ :: #121 이메일 입력 페이지 리펙토링

--- Mukgen/Mukgen-iOS/Sources/Application/SceneDelegate.swift
+++ Mukgen/Mukgen-iOS/Sources/Application/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let moduleFactory = ModuleFactory.shared
 
-        let rootViewController = moduleFactory.StartMukgenVC()
+        let rootViewController = moduleFactory.InputEmailVC()
 //        let navigationController = UINavigationController(rootViewController: rootViewController)
         
         window?.rootViewController = rootViewController
