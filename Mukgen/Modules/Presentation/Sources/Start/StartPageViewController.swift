5047d6a1fed529091b542ab26fad215e340f85af
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-08-19 14:10:59 +0900

    ♻️ :: #121 시작 페이지 리펙토링

--- Mukgen/Modules/Presentation/Sources/Start/StartPageViewController.swift
+++ Mukgen/Modules/Presentation/Sources/Start/StartPageViewController.swift
@@ -4,68 +4,44 @@ import Then
 import MukgenKit
 import Core
 
-public class StartPageViewController: BaseVC {
+public class StartPageViewController: BaseSV {
     
     public var factory: ModuleFactoryInterface!
-        
-    private let startButton = CustomButton(title: "시작하기",
-                                           backgroundColor: MukgenKitAsset.Colors.pointBase.color,
-                                           titleColor: UIColor.white,
-                                           font: UIFont.systemFont(ofSize: 16, weight: .semibold))
     
-    private let loginButton = CustomButton(title: "로그인",
-                                           backgroundColor: MukgenKitAsset.Colors.primaryLight3.color, titleColor: UIColor.black,
-                                           font: UIFont.systemFont(ofSize: 16, weight: .semibold))
-    
-    private let whiteBox = UILabel().then {
-        $0.backgroundColor = .white
-    }
-    
-    private let mukgenLogo = UIImageView().then {
-        $0.image = MukgenKitAsset.Images.mukgenIcon.image
-    }
-    
-    public override func layout() {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
         
-        view.addSubviews([whiteBox,
-                          startButton,
-                          loginButton,
-                          mukgenLogo])
+        updateWith(self)
         
-        mukgenLogo.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview().offset(-79.0)
-            $0.width.equalTo(160.0)
-            $0.height.equalTo(189.0)
-        }
-        whiteBox.snp.makeConstraints {
-            $0.width.equalToSuperview()
-            $0.height.equalTo(192.0)
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview()
-        }
-        
-        loginButton.snp.makeConstraints {
-            $0.centerX.equalTo(whiteBox.snp.centerX)
-            $0.top.equalTo(whiteBox.snp.top).offset(24.0)
-            $0.width.equalToSuperview().inset(20.0)
-            $0.height.equalTo(55.0)
-        }
-        
-        startButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(loginButton.snp.bottom).offset(24)
-            $0.width.equalToSuperview().inset(20.0)
-            $0.height.equalTo(55.0)
-        }
-    }
-    
-    public override func attribute() {
         view.backgroundColor = MukgenKitAsset.Colors.primaryLight3.color
-        startButton.addTarget(self, action: #selector(startButtonDidTap), for: .touchUpInside)
-    }
-    
-    @objc func startButtonDidTap(_ sender: Any) {
-        self.navigationController?.pushViewController(StartMukgenViewController(), animated: true)
+        
+        mukgenLogo.image = MukgenKitAsset.Images.mukgenIcon.image
+        
+        buttonBackView.backgroundColor = .white
+        
+        firstButton = CustomButton(title: "로그인",
+                                    backgroundColor: MukgenKitAsset.Colors.primaryLight3.color, titleColor: UIColor.black,
+                                    font: UIFont.systemFont(ofSize: 16, weight: .semibold))
+        
+        secondButton = CustomButton(title: "시작하기",
+                                   backgroundColor: MukgenKitAsset.Colors.pointBase.color,
+                                   titleColor: UIColor.white,
+                                   font: UIFont.systemFont(ofSize: 16, weight: .semibold))
+        
+        view.addSubviews([mukgenLogo, buttonBackView, firstButton, secondButton])
+        
+        firstButton.snp.makeConstraints {
+            $0.centerX.equalTo(buttonBackView.snp.centerX)
+            $0.top.equalTo(buttonBackView.snp.top).offset(24.0)
+            $0.width.equalToSuperview().inset(20.0)
+            $0.height.equalTo(55.0)
+        }
+        
+        secondButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(firstButton.snp.bottom).offset(24)
+            $0.width.equalToSuperview().inset(20.0)
+            $0.height.equalTo(55.0)
+        }
     }
 }
