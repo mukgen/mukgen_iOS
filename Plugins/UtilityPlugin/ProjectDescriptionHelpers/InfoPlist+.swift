92596943f54e9d7d71643169f446e6320912f2b6
junha <goodjunha@gmail.com>
2023-09-07 22:21:16 +0900

    🔥:: 안쓰는 파일 삭제

--- Plugins/UtilityPlugin/ProjectDescriptionHelpers/InfoPlist+.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import ProjectDescription
-
-public extension ProjectDescription.InfoPlist {
-
-    static let `defaultFile`: ProjectDescription.InfoPlist = .file(path: .relativeToRoot("Support/Info.plist"))
-}
