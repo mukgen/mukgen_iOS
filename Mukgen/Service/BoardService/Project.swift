d1c6ebb899591c0a21f77c33c57023985fab0824
junha <goodjunha@gmail.com>
2023-10-10 15:25:25 +0900

    ♻️:: #126 서비스 필요없는 라이브러리 삭제

--- Mukgen/Service/BoardService/Project.swift
+++ Mukgen/Service/BoardService/Project.swift
@@ -6,13 +6,6 @@ let project = Project.makeModule(
     name: "BoardService",
     product: .staticFramework,
     dependencies: [
-        .Project.thirdPartyLib,
-        .SPM.SnapKit,
-        .SPM.Then,
-        .SPM.Moya,
-        .SPM.RxCocoa,
-        .SPM.RxMoya,
-        .SPM.RxSwift,
-        .SPM.RxRelay
+        .Project.thirdPartyLib
     ]
 )
