4524cddcdfdb3fe93583cf6123ddc000d2fca298
junha <goodjunha@gmail.com>
2023-10-10 15:26:27 +0900

    🔥:: 필요없는 의존성 삭제

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,7 +6,6 @@ let spm = SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.0")),
     .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
     .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0")),
-//    .remote(url: "https://github.com/RxSwiftCommunity/RxNimble", requirement: .upToNextMajor(from: "5.1.0")),
     .remote(url: "https://github.com/ReactiveX/RxSwift", requirement: .upToNextMajor(from: "6.5.0"))
 ])
 
