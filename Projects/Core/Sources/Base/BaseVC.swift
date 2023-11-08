8acefcacb3a1414639a6118913992009300c0e14
junha <goodjunha@gmail.com>
2023-11-08 19:20:50 +0900

    ♻️:: [#132] Core 수정

---
+++ Projects/Core/Sources/Base/BaseVC.swift
@@ -0,0 +1,28 @@
+import UIKit
+import SnapKit
+import Then
+import Foundation
+
+open class BaseVC: UIViewController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        attribute()
+        hideKeyboardWhenTappedArround()
+    }
+    
+    open func hideKeyboardWhenTappedArround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard) )
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    open func layout() {}
+    open func attribute() {}
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
