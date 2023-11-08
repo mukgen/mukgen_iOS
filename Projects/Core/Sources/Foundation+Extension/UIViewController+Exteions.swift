fe8748236a178cec16d88427c9ea57127e783325
junha <goodjunha@gmail.com>
2023-11-08 19:49:12 +0900

    ♻️:: [#132] 에셋 이미지 추가

--- Projects/Core/Sources/Foundation+Extension/UIViewController+Exteions.swift
+++ Projects/Core/Sources/Foundation+Extension/UIViewController+Exteions.swift
@@ -3,15 +3,15 @@ import SnapKit
 import Then
 
 extension UIViewController {
-    func showAlert(labelText: String ,buttonLabelText: String, Botton Action:@escaping ()->Void ) {
-        guard let window = UIApplication.shared.windows.first else { return }
-        
-        let alertView = CustomAlertView(labelText: labelText, buttonLabelText: buttonLabelText, buttonAction: Action)
-        
-        window.addSubview(alertView)
-        
-        alertView.snp.makeConstraints{
-            $0.edges.equalToSuperview()
-        }
-    }
+//    func showAlert(labelText: String ,buttonLabelText: String, Botton Action:@escaping ()->Void ) {
+//        guard let window = UIApplication.shared.windows.first else { return }
+//        
+//        let alertView = CustomAlertView(labelText: labelText, buttonLabelText: buttonLabelText, buttonAction: Action)
+//        
+//        window.addSubview(alertView)
+//        
+//        alertView.snp.makeConstraints{
+//            $0.edges.equalToSuperview()
+//        }
+//    }
 }
