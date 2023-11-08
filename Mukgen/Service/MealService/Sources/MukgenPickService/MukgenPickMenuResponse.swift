ea380d48033784be92378b537e42c01c41703322
junha <goodjunha@gmail.com>
2023-11-08 10:08:58 +0900

    🔥:: [#132] 안쓰는 거 삭제

--- Mukgen/Service/MealService/Sources/MukgenPickService/MukgenPickMenuResponse.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public struct MukgenPickMenuResponse: Codable {
-    public let month: Int
-    public let day: Int
-    public let riceType: String
-    public let riceId: Int
-}
