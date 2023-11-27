6e4a910d9c3832ecbb43ed6e5075d26d8ff86c47
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-27 14:23:52 +0900

    ✨ :: [#135] title UILabel 제작

--- Projects/Modules/MukgenKit/Sources/DesignSystem/UILabel/Auth/CustomTitleUILabel.swift
+++ Projects/Modules/MukgenKit/Sources/DesignSystem/UILabel/Auth/CustomTitleUILabel.swift
@@ -1,9 +1,50 @@
-//
-//  CustomTitle.swift
-//  MukgenKit
-//
-//  Created by 이은호 on 11/27/23.
-//  Copyright © 2023 Mukgen. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+open class CustomTitleUILabel: UILabel {
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    }
+    
+    public init (
+        text: String,
+        lineNumber: Int? = 1
+    ) {
+        super.init(frame: .zero)
+        
+        titleLabel.text = text
+        titleLabel.numberOfLines = lineNumber ?? 1
+        
+        setupUI(line: lineNumber ?? 1)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(line: Int) {
+        switch line {
+        case 1:
+            titleLabel.snp.makeConstraints {
+                $0.height.equalTo(29.0)
+            }
+            break
+        case 2:
+            titleLabel.snp.makeConstraints {
+                $0.height.equalTo(58.0)
+            }
+            break
+        default:
+            break
+        }
+        
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
